a4b5501ffd4a32b3d92ce0f34a315d42f4107073
Davide Sforza <sforza.1710262@studenti.uniroma1.it>
2021-09-13 19:15:43 +0200

    Changed default tables sizes

--- scripts/dielectrics.py
+++ scripts/dielectrics.py
@@ -94,7 +94,7 @@ def integrand(argc, argv):
 
 if __name__ == '__main__':
   parser = ArgumentParser()
-  parser.add_argument('--size', help='Look-up table size', type=int, default=16)
+  parser.add_argument('--size', help='Look-up table size', type=int, default=32)
   parser.add_argument('--output', help='Output filename', default='dielectrics.csv')
 
   args = parser.parse_args()
